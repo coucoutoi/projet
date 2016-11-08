568dd8d2d6ca202fe869409a1b5c667d0549602b
alexandre hulsken <alexandre hulsken>
2016-11-08 20:04:56 +0100

    renale the module solver

---
+++ src/sudoku_solver.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+:mod:`solver` module
+
+:author: HULSKEN Alexandre & KARTI Adeniss
+
+:date: 2016, november
+
+This module provides sudoku solver's primitive operations
+
+:Provides:
+
+* `print_grid`
+* `MAJ_hipothetic`
+"""
+
+
+import sudoku_grid,cells
+
+def print_grid(grid):
+    """
+    :Examples:
+    >>> grid = sudoku_grid.make_grid(sudoku_grid.val_test)
+    >>> print_grid(grid)
+    +-------+-------+-------+
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    +-------+-------+-------+
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    +-------+-------+-------+
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    | . 1 2 | 3 4 5 | 6 7 8 |
+    +-------+-------+-------+
+    """
+    print('+'+'-------+'*3)
+    for l1 in range(3):
+        for l2 in range(3):
+            print('|',end='')
+            for c1 in range(3):
+                for c2 in range(3):
+                    if cells.get_cellvalue(sudoku_grid.get_cell(grid,l1*3+l2,c1*3+c2)):
+                        print(' {:d}'.format(cells.get_cellvalue(sudoku_grid.get_cell(grid,l1*3+l2,c1*3+c2))),end='')
+                    else:
+                        print(' .',end='')
+                print(' |',end='')
+            print()
+        print('+'+'-------+'*3)
+
+def MAJ_hipothetic(grid,nthline,nthcol,set_value):
+    line = sudoku_grid.get_line(grid,nthline)
+    col = sudoku_grid.get_colomn(grid,nthcol)
+    """square = sudoku_grid.get_square(grid,)"""
+    for k in [line,col]:
+        for c in k:
+            cells.unset_cellhipothetic(c,set_value)
+            if len(cell.get_cellhipo(c)) == 1:
+                cells.set_cellvalue(c,cell.get_cellhipo(c).pop())
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod
